857ccb7f9a6fdf93a224ab7518299e40f609b056
itsmeeSanj <itsmeesanj@gmail.com>
2026-06-23 15:15:10 -0400

    feat: add MongoDB connection config

---
+++ server/config/mongodb.js
@@ -0,0 +1,14 @@
+// database connection
+import mongoose from "mongoose";
+
+export default async function connectDB() {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log(`MongoDB Connected: ${mongoose.connection.host}`);
+    });
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+}
