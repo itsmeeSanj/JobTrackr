a3765f0b88e3dfd0a80d14bbd10b7c37afdcbdcc
itsmeeSanj <itsmeesanj@gmail.com>
2026-07-11 13:26:57 -0400

    feat:complete auth routes with public and protected seperation

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,6 +5,7 @@ import {
   logout,
   sendVerifyOtp,
   verifyEmail,
+  sendResetOtp,
   verifyResetOtp,
   resetPassword,
 } from "../controllers/authController.js";
@@ -12,15 +13,16 @@ import userAuth from "../middleware/userAuth.js";
 
 const authRouter = express.Router();
 
-// ── Public routes (no auth required) ────────────────
+//  Public routes (no auth required)
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
-authRouter.post("/send-reset-otp", sendVerifyOtp); // ← no userAuth
+authRouter.post("/send-reset-otp", sendResetOtp);
 authRouter.post("/verify-reset-otp", verifyResetOtp);
 authRouter.post("/reset-password", resetPassword);
 
-// ── Protected routes (auth required) ────────────────
+//  Protected routes (auth required)
+authRouter.post("/send-verify-otp", userAuth, sendVerifyOtp);
 authRouter.post("/verify-email", userAuth, verifyEmail);
 authRouter.get("/is-auth", userAuth, (req, res) => {
   res.json({ success: true, message: "Authenticated" });
