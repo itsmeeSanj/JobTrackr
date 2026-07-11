506b5c6a5d5ffddb8a6c5f3ae391bf4e8a3bc49c
itsmeeSanj <itsmeesanj@gmail.com>
2026-07-11 13:03:12 -0400

    feat: add job routes with auth protection

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -1,7 +1,22 @@
 import express from "express";
+import userAuth from "../middleware/userAuth.js";
+import {
+  addJob,
+  getJobs,
+  getJob,
+  updateJob,
+  deleteJob,
+  getStats,
+} from "../controllers/jobController.js";
 
 const jobRouter = express.Router();
 
-// routes coming soon
+// get all routes protected
+jobRouter.get("/stats", userAuth, getStats); // ← must be BEFORE /:id
+jobRouter.get("/", userAuth, getJobs);
+jobRouter.post("/", userAuth, addJob);
+jobRouter.get("/:id", userAuth, getJob);
+jobRouter.put("/:id", userAuth, updateJob);
+jobRouter.delete("/:id", userAuth, deleteJob);
 
 export default jobRouter;
