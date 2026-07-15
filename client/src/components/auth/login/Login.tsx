1aea08c280d714fb74f25e0bba58452d5f3657fd
itsmeeSanj <itsmeesanj@gmail.com>
2026-07-15 14:27:05 -0400

    feat:add login design

--- client/src/components/auth/login/Login.tsx
+++ client/src/components/auth/login/Login.tsx
@@ -1,5 +1,70 @@
+import { Button, Checkbox, Col, Form, Input, Row } from "antd";
+
 function Login() {
-  return <div>Login</div>;
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div>
+      <Row>
+        <Col span={12}>
+          <div className='bg-blue'>Login</div>
+        </Col>
+        <Col span={12}>
+          <div>
+            <Form
+              name='basic'
+              labelCol={{ span: 8 }}
+              wrapperCol={{ span: 16 }}
+              style={{ maxWidth: 600 }}
+              initialValues={{ remember: true }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete='off'
+            >
+              <Form.Item
+                label='Email'
+                name='email'
+                rules={[
+                  {
+                    required: true,
+                    message: "Please enter a valid email address",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label='Password'
+                name='password'
+                rules={[
+                  { required: true, message: "Please input your password!" },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item name='remember' valuePropName='checked' label={null}>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <Form.Item label={null}>
+                <Button type='primary' htmlType='submit'>
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Col>
+      </Row>
+      {/*  */}
+    </div>
+  );
 }
 
 export default Login;
