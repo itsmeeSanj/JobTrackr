32eefa55737d448d9c06e6d348db3ce3ac3f1e9b
itsmeeSanj <itsmeesanj@gmail.com>
2026-07-28 00:31:08 -0400

    update: change sidebar UI

--- client/src/layout/components/Sidebar.tsx
+++ client/src/layout/components/Sidebar.tsx
@@ -51,6 +51,7 @@ export default function Sidebar({ collapsed }: Props) {
       trigger={null}
       collapsible
       collapsed={collapsed}
+      width={230}
       style={{
         overflow: "auto",
         height: "100vh",
@@ -63,31 +64,27 @@ export default function Sidebar({ collapsed }: Props) {
       {/* Logo */}
       <div
         style={{
-          height: 56,
-          margin: "12px 16px",
-          borderRadius: 10,
-          background: "#4F46E5",
+          minHeight: 56,
+          margin: "0 0 12px ",
+          padding: "16px 16px",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
           overflow: "hidden",
+          borderBottom: "1px solid rgba(255,255,255,.6)",
         }}
       >
         {collapsed ? (
-          <span style={{ color: "#fff", fontWeight: 700, fontSize: 18 }}>
-            J
-          </span>
+          <span className='text-white font-bold text-xl'>J</span>
         ) : (
-          <span style={{ color: "#fff", fontWeight: 700, fontSize: 16 }}>
-            JobTrackr
-          </span>
+          <span className='text-white font-bold text-xl'>JobTrackr</span>
         )}
       </div>
 
       {/* Nav items */}
       <Menu
         mode='inline'
-        className='bg-[#4F46E5]!'
+        className='bg-[#4F46E5]! '
         selectedKeys={[location.pathname]} // ← highlights active page
         items={menuItems}
         onClick={({ key }) => navigate(key)} // ← navigates on click
