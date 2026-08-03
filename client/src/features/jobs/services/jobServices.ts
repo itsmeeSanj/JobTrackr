6aec92f4465627b307e90135ef3e199fb2005966
itsmeeSanj <itsmeesanj@gmail.com>
2026-08-02 23:07:31 -0400

    feat: added jobservice

---
+++ client/src/features/jobs/services/jobServices.ts
@@ -0,0 +1,30 @@
+// src/services/jobService.ts
+import api from "../../../lib/axios";
+import type { Job, JobStats } from "../../../types/job.types";
+
+export const getJobs = async (): Promise<Job[]> => {
+  const res = await api.get("/api/jobs");
+  return res.data.jobs;
+};
+
+export const getStats = async (): Promise<JobStats> => {
+  const res = await api.get("/api/jobs/stats");
+  return res.data.stats;
+};
+
+export const addJob = async (job: Partial<Job>): Promise<Job> => {
+  const res = await api.post("/api/jobs", job);
+  return res.data.job;
+};
+
+export const updateJob = async (
+  id: string,
+  job: Partial<Job>,
+): Promise<Job> => {
+  const res = await api.put(`/api/jobs/${id}`, job);
+  return res.data.job;
+};
+
+export const deleteJob = async (id: string): Promise<void> => {
+  await api.delete(`/api/jobs/${id}`);
+};
