8063db5a4e2aa35fedc761c3b4e4aa077251d8de
itsmeeSanj <itsmeesanj@gmail.com>
2026-08-03 04:41:49 -0400

    feat:add jobTypesConstant

---
+++ client/src/constants/jobTypes.ts
@@ -0,0 +1,7 @@
+export const JOB_TYPE_OPTIONS = [
+  { label: "Full-time", value: "Full-time" },
+  { label: "Part-time", value: "Part-time" },
+  { label: "Contract", value: "Contract" },
+  { label: "Remote", value: "Remote" },
+  { label: "Hybrid", value: "Hybrid" },
+];
